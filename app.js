0f0169008f43e1bbfbc9daf8200e530698c594c5
darco2018 <newsecond@outlook.com>
2019-09-18 12:14:27 +0200

    Add views/index.ejs

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Snippets HOME page');
+  // res.send('Welcome to Snippets HOME page');
+  res.render('index');
 });
 
 
