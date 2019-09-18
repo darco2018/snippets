7e2ba5bc72b8509cc838a1c8159b8e36fea703a8
darco2018 <newsecond@outlook.com>
2019-09-18 14:06:03 +0200

    Can add snippet by post

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: 'true' }));
 
 const inMemoryDb = [
   {
@@ -33,6 +35,40 @@ const inMemoryDb = [
 app.get('/', (req, res) => {
   // res.send('Welcome to Snippets HOME page');
   res.render('index', { snippets: inMemoryDb });
+});
+
+
+app.post('/addsnippet', (req, res) => {
+  console.log('POST sends a snippet...');
+  console.log(req.body);
+
+  /* const newMockSnippet = {
+    id: inMemoryDb.length + 1,
+    title: 'Start express/node app',
+    content: 'snippet proper default',
+    lg: 'lg default',
+    desc: 'default description',
+  }; */
+  const newId = inMemoryDb.length + 1;
+  const newTitle = !req.body.title ? `Snippet nr ${newId}` : req.body.title;
+  const newContent = !req.body.content ? 'No content' : req.body.content;
+  const newLg = !req.body.lg ? 'Unknown' : req.body.lg;
+  const newDesc = !req.body.desc ? 'No description' : req.body.desc;
+
+  const newSnippet = {
+    id: newId,
+    title: newTitle,
+    content: newContent,
+    lg: newLg,
+    desc: newDesc,
+  };
+  inMemoryDb.push(newSnippet);
+
+  res.redirect('/');
+});
+
+app.get('*', (req, res) => {
+  res.send('Oops, sth went wrong!');
 });
 
 
