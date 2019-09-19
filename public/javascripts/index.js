6043e263dd0fb65a9c1f14f3fadcb4e1af973473
darco2018 <newsecond@outlook.com>
2019-09-19 19:41:27 +0200

    Add Bootstrap & JQuery

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-console */
 
+$(document).ready(() => {
+  const element = $('meta[name="active-menu"]').attr('content');
+  $(`#${element}`).addClass('active');
+});
+
 document.getElementById('deleteButton').addEventListener('click', () => {
   const idInput = document.getElementById('idInput');
   const id = idInput.value;
